110cc89c530bea4a8b6440b96e005235798bd946
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-02-16 20:18:06 -0600

    added functionality for validating file input in sign up form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -7,10 +7,40 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail, thumbnailError);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selectedFile = e.target.files[0];
+    console.log(selectedFile);
+
+    if (!selectedFile) {
+      setThumbnailError("Please select a file.");
+      return;
+    }
+    if (!selectedFile.type.includes("image")) {
+      setThumbnailError("Please select image files only");
+      return;
+    }
+
+    if (selectedFile.size > 200000) {
+      setThumbnailError("The image size must be less than 200kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selectedFile);
+    console.log("Thumbnail updated");
+  };
 
   return (
     <div>
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
         <label>
           <span>Email: </span>
@@ -44,7 +74,8 @@ export default function Signup() {
 
         <label>
           <span>Profile Picture:</span>
-          <input type="file" required />
+          <input type="file" required onChange={handleFileChange} />
+          {thumbnailError && <div className="error">{thumbnailError}</div>}
         </label>
 
         <button className="btn">Sign Up</button>
