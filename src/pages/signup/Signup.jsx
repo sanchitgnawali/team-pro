36d55a3e4e056a324ca22dcf1311acd264bff64a
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-02-16 19:04:04 -0600

    Created signup form and added styles to it

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <div>
+      <form className="auth-form">
+        <h2>Sign Up</h2>
+        <label>
+          <span>Email: </span>
+          <input
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+
+        <label>
+          <span>Password: </span>
+          <input
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+
+        <label>
+          <span>Display Name: </span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+
+        <label>
+          <span>Profile Picture:</span>
+          <input type="file" required />
+        </label>
+
+        <button className="btn">Sign Up</button>
+      </form>
+    </div>
+  );
 }
