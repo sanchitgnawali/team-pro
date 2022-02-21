cecfcd470414b0039facaf7ff953519b710d9312
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-02-21 14:00:02 -0600

    Made users to show offline when they sign out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "./../hooks/useLogout";
 
+// stylesheet
 import "./Navbar.css";
 
+//icons
 import Temple from "./../assets/temple.svg";
+
 const Navbar = () => {
+  const { isPending, logout } = useLogout();
   return (
     <div className="navbar">
       <ul>
@@ -20,7 +25,16 @@ const Navbar = () => {
           <Link to="signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              Logging Out
+            </button>
+          )}
         </li>
       </ul>
     </div>
