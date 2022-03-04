873b18944307e368572f3a39675cf7b02c7023cb
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-03-03 18:04:43 -0600

    Created useDocument custom hook and created ProjectList component to list all the projects

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id);
+
+    const unsub = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError("ERROR: couldn' fetch the data");
+      }
+    );
+
+    return () => unsub();
+  }, [collection, id]);
+
+  return { document, error };
+};
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,17 @@
+import Projectlist from "../../components/ProjectList";
+import { useCollection } from "./../../hooks/useCollection";
+
+// styles
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <Projectlist projects={documents} />}
+    </div>
+  );
 }
